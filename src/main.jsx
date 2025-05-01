d272ce589f01ad2508bcea175a876b607aa09457
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-05-02 01:59:59 +0600

    working on blog card

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { router } from './router/Router.jsx'
-import HomePage from './pages/HomePage.jsx'
-import Blogs from './pages/Blogs.jsx'
-import Bookmarks from './pages/Bookmarks.jsx'
-import MainLayout from './layout/MainLayout.jsx'
-
-
+import HomePage from "./pages/HomePage.jsx";
+import Blogs from "./pages/Blogs.jsx";
+import Bookmarks from "./pages/Bookmarks.jsx";
+import MainLayout from "./layout/MainLayout.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,21 +16,22 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <HomePage />
+        element: <HomePage />,
       },
       {
         path: "/blogs",
         element: <Blogs />,
+        loader: () => fetch("https://dev.to/api/articles?per_page=30&top=7"),
       },
       {
         path: "/bookmarks",
-        element: <Bookmarks />
-      }
-    ]
-  }
-])
-createRoot(document.getElementById('root')).render(
+        element: <Bookmarks />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
