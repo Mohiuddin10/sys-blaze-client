911fe0de15e0e710fa43033ab6ede369fa43f0d4
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-05-04 13:09:18 +0600

    working of blog nested route

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,8 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Blog = () => {
+  const [tabIndex, setTabIndex] = useState(0);
   const blog = useLoaderData();
   const {
     title,
@@ -36,8 +38,53 @@ const Blog = () => {
               {comments_count} comments • {public_reactions_count} reactions
             </p>
           </div>
+          {/* tab start  */}
+          <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap">
+            <Link
+              onClick={() => setTabIndex(0)}
+              className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${
+                tabIndex === 0 ? "border border-b-0" : "border-b"
+              } rounded-t-lg border-gray-400`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <span>Content</span>
+            </Link>
+            <Link
+              onClick={() => setTabIndex(1)}
+              className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${
+                tabIndex === 1 ? "border border-b-0" : "border-b"
+              } rounded-t-lg border-gray-400`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+              </svg>
+              <span>Author</span>
+            </Link>
+          </div>
+          {/* tab end  */}
         </div>
-        <div className="text-gray-100">
+        <div className="">
           <p>Insert the actual text content here...</p>
         </div>
       </article>
